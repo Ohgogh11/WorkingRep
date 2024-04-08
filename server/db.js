7ecf3c422cb081b44332a762684063648f207ec1
Itay Fridburg <itay.fridburg@gmail.com>
2024-04-08 19:27:43 +0300

    Updated db.js

--- server/db.js
+++ server/db.js
@@ -2,17 +2,17 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const pool = mysql.createPool({
-//     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE
-// }).promise()
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Ohgogh5512',
-    database: 'alphaman'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise()
+// const pool = mysql.createPool({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'Ohgogh5512',
+//     database: 'alphaman'
+// }).promise()
 
 module.exports = pool;
